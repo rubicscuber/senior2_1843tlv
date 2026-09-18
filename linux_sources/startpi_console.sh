3115d4b1b549b7ecee241bd6f52f5dd93f44adb7
rubicscuber <nathanielroberts96@gmail.com>
2026-09-18 11:57:50 -0400

    modified startpi_console.sh

--- linux_sources/startpi_console.sh
+++ linux_sources/startpi_console.sh
@@ -1,1 +1,2 @@
-./console_only_Pi -g ../chirp_configs/18xx_traffic_monitoring_70m_MIMO_3D.cfg -c /dev/ttyACM0 -d /dev/ttyACM1
+./console_only_Pi -g ../chirp_configs/18xx_traffic_monitoring_70m_MIMO_2D.cfg -c /dev/ttyACM0 -d /dev/ttyACM1
+#./console_only_Pi -g ../chirp_configs/IWR_1843BOOST_bike.cfg -c /dev/ttyACM0 -d /dev/ttyACM1
